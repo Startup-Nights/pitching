33d4c40830ff78307324cef5ac1915cd5f02d7f6
Mischa Jörg <mischa@ninetyfour.ch>
2024-06-17 17:12:23 +0200

    setup basic navigation and pages

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,25 +1,6 @@
-import { Resource } from "sst";
-import { json } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-
-
-export async function loader() {
-  const command = new PutObjectCommand({
-    Key: crypto.randomUUID(),
-    Bucket: Resource.PitchingSessionsBucket.name,
-  });
-  const url = await getSignedUrl(new S3Client({
-    region: 'us-east-1',
-  }), command);
-
-  return json({ url });
-}
-
 export default function Index() {
-  const data = useLoaderData<typeof loader>();
   return (
-    <>index</>
+    <div className="xl:pl-72">
+    </div>
   );
 }
