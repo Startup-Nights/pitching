b42636f1d17a36052c6a73f5df5138349b5fc561
Mischa Jörg <mischa@ninetyfour.ch>
2024-06-30 19:55:16 +0200

    signup: add additional questions
    Signed-off-by: Mischa Jörg <mischa@ninetyfour.ch>

--- app/routes/signup.tsx
+++ app/routes/signup.tsx
@@ -104,7 +104,7 @@ export default function Signup() {
 
                 <div className="col-span-full">
                   <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-white">
-                    Pitchdeck
+                    Pitchdeck (optional)
                   </label>
                   <div className="mt-2 flex justify-center rounded-lg border border-dashed border-white/25 px-6 py-10">
                     <div className="text-center">
@@ -115,7 +115,7 @@ export default function Signup() {
                           className="relative cursor-pointer rounded-md bg-gray-900 font-semibold text-white focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 focus-within:ring-offset-gray-900 hover:text-indigo-500"
                         >
                           <span>Upload a file</span>
-                          <input id="pitchdeck" required name="pitchdeck" type="file" className="sr-only" />
+                          <input id="pitchdeck" name="pitchdeck" type="file" className="sr-only" />
                         </label>
                         <p className="pl-1">or drag and drop</p>
                       </div>
@@ -123,6 +123,105 @@ export default function Signup() {
                     </div>
                   </div>
                 </div>
+
+                <div className="sm:col-span-6">
+                  <label htmlFor="problem_description" className="block text-sm font-medium leading-6 text-white">
+                    What problem do you address?
+                  </label>
+                  <div className="mt-2">
+                    <textarea
+                      id="problem_description"
+                      required
+                      name="problem_description"
+                      className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-6">
+                  <label htmlFor="problem_solution" className="block text-sm font-medium leading-6 text-white">
+                    How do you solve this problem?
+                  </label>
+                  <div className="mt-2">
+                    <textarea
+                      id="problem_solution"
+                      required
+                      name="problem_solution"
+                      className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-6">
+                  <label htmlFor="problem_approach" className="block text-sm font-medium leading-6 text-white">
+                    What is unique about your problem-solving approach?
+                  </label>
+                  <div className="mt-2">
+                    <textarea
+                      id="problem_approach"
+                      required
+                      name="problem_approach"
+                      className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-6">
+                  <label htmlFor="userbase" className="block text-sm font-medium leading-6 text-white">
+                    Who is your user and who is your paying customer (note: may be the same group)?
+                  </label>
+                  <div className="mt-2">
+                    <textarea
+                      id="userbase"
+                      required
+                      name="userbase"
+                      className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-6">
+                  <label htmlFor="revenue" className="block text-sm font-medium leading-6 text-white">
+                    What is your current revenue to date? If none, how do you plan to make money?
+                  </label>
+                  <div className="mt-2">
+                    <textarea
+                      id="revenue"
+                      required
+                      name="revenue"
+                      className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-6">
+                  <label htmlFor="advantage" className="block text-sm font-medium leading-6 text-white">
+                    What is your competitive advantage?
+                  </label>
+                  <div className="mt-2">
+                    <textarea
+                      id="advantage"
+                      required
+                      name="advantage"
+                      className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-6">
+                  <label htmlFor="money_usage" className="block text-sm font-medium leading-6 text-white">
+                    If you won USD 1 Mio., how would you spend it?
+                  </label>
+                  <div className="mt-2">
+                    <textarea
+                      id="money_usage"
+                      required
+                      name="money_usage"
+                      className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
               </div>
             </div>
 
@@ -180,7 +279,7 @@ export default function Signup() {
                 </div>
 
                 <div className="sm:col-span-3">
-                  <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">
+                  <label htmlFor="linkedin" className="block text-sm font-medium leading-6 text-white">
                     Linkedin
                   </label>
                   <div className="mt-2">
@@ -194,6 +293,21 @@ export default function Signup() {
                   </div>
                 </div>
 
+                <div className="sm:col-span-3">
+                  <label htmlFor="country" className="block text-sm font-medium leading-6 text-white">
+                    Country
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="country"
+                      required
+                      name="country"
+                      type="text"
+                      className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
               </div>
             </div>
 
@@ -201,7 +315,6 @@ export default function Signup() {
               <div className="mt-10 space-y-10">
                 <fieldset>
                   <legend className="text-sm font-semibold leading-6 text-white">Which round do you apply for?</legend>
-                  <p className="mt-1 text-sm leading-6 text-gray-400">TODO: give some pointer what is considered seed / pre-seed.</p>
                   <div className="mt-6 space-y-6">
                     <div className="flex items-center gap-x-3">
                       <input
