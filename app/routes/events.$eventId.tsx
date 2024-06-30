5d0a7f4cefe3f4fc5bd6724ab886c760eee5cec8
Mischa Jörg <mischa@ninetyfour.ch>
2024-06-30 21:13:28 +0200

    events: try to stream data
    Signed-off-by: Mischa Jörg <mischa@ninetyfour.ch>

--- app/routes/events.$eventId.tsx
+++ app/routes/events.$eventId.tsx
@@ -1,30 +1,17 @@
 import { Resource } from "sst"
-import { json, useLoaderData } from "@remix-run/react";
+import { Await, defer, json, useLoaderData } from "@remix-run/react";
+import { Suspense } from "react";
+import { LoaderFunctionArgs } from "@remix-run/node";
 
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ')
-}
-
-export const loader = async ({ params }: any) => {
-  const raw = await fetch(Resource.GetRegistrations.url);
-  const data = await raw.json()
-  const registrations: Registration[] = data ? data : []
-  return json({
+export async function loader({ params }: LoaderFunctionArgs) {
+  return defer({
     name: params.eventId,
-    registrations: registrations
+    registrations: (await fetch(Resource.GetRegistrations.url)).json()
   })
 };
 
 export default function Index({ }) {
   const event = useLoaderData<typeof loader>()
-
-  const filtered_registrations = event.registrations.filter((item) => item.round === event.name)
-  const approved_registrations = event.registrations.filter((item) => item.approved)
-
-  const stats = [
-    { name: 'Number of applications', value: filtered_registrations.length, unit: '' },
-    { name: 'Approval rate', value: (approved_registrations.length / Math.max(event.registrations.length, 1) * 100), unit: '%' },
-  ]
 
   return (
     <div className="xl:pl-72">
@@ -43,74 +30,89 @@ export default function Index({ }) {
           </div>
         </header>
 
-        {/* Activity list */}
-        <div className="border-t border-white/10 pt-11">
-          <h2 className="px-4 text-base font-semibold leading-7 text-white sm:px-6 lg:px-8">List of applications</h2>
-          <table className="mt-6 w-full whitespace-nowrap text-left">
-            <colgroup>
-              <col className="lg:w-2/12" />
-              <col className="lg:w-2/12" />
-              <col className="lg:w-2/12" />
-              <col className="lg:w-2/12" />
-              <col className="lg:w-2/12" />
-            </colgroup>
-            <thead className="border-b border-white/10 text-sm leading-6 text-white">
-              <tr>
-                <th scope="col" className="py-2 pl-4 pr-8 font-semibold sm:pl-6 lg:pl-8">
-                  Name
-                </th>
-                <th scope="col" className="py-2 pl-0 pr-8 font-semibold sm:pl-6 lg:pl-8">
-                  Company
-                </th>
-                <th scope="col" className="py-2 pl-0 pr-4 font-semibold sm:pl-6 lg:pl-8">
-                  Website
-                </th>
-                <th scope="col" className="py-2 pl-0 pr-8 font-semibold sm:pl-6 lg:pl-8">
-                  Pitching Deck
-                </th>
-                <th scope="col" className="py-2 pl-0 pr-4 font-semibold sm:pl-6 lg:pl-8">
-                  Applied On
-                </th>
-              </tr>
-            </thead>
-            <tbody className="divide-y divide-white/5">
-              {filtered_registrations.map((item, i) => (
-                <tr key={i}>
-                  <td className="py-4 pl-4 pr-8 sm:pl-6 lg:pl-8">
-                    <div className="flex items-center">
-                      <div className="truncate overflow-hidden text-sm font-medium leading-6 text-white">{item.firstname} {item.lastname}</div>
-                    </div>
-                  </td>
-                  <td className="py-4 pl-4 pr-8 sm:pl-6 lg:pl-8">
-                    <div className="flex items-center">
-                      <div className="truncate overflow-hidden text-sm font-medium leading-6 text-white">{item.company}</div>
-                    </div>
-                  </td>
-                  <td className="py-4 pl-4 pr-8 sm:pl-6 lg:pl-8">
-                    <div className="flex items-center">
-                      <div className="truncate overflow-hidden text-sm font-medium leading-6 text-white underline">
-                        <a href={item.website} target="_blank">{item.website}</a>
-                      </div>
-                    </div>
-                  </td>
-                  <td className="py-4 pl-4 pr-8 sm:pl-6 lg:pl-8">
-                    <div className="flex items-center">
-                      <div className="truncate overflow-hidden text-sm font-medium leading-6 text-white underline">
-                        <a href={item.pitching_deck} target="_blank">Link</a>
-                      </div>
-                    </div>
-                  </td>
-                  <td className="py-4 pl-4 pr-8 sm:pl-6 lg:pl-8">
-                    <div className="flex items-center">
-                      <div className="truncate overflow-hidden text-sm font-medium leading-6 text-white">{item.applied_on}</div>
-                    </div>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Await resolve={event.registrations}>
+            {(e) => <EventTable event={e} name={event.name} />}
+          </Await>
+        </Suspense>
       </main>
     </div >
+  )
+}
+
+
+function EventTable({ event, name }: any) {
+  const filtered_registrations = event.filter((item: any) => item.round === name)
+
+  return (
+    <>
+      {/* Activity list */}
+      < div className="border-t border-white/10 pt-11" >
+        <h2 className="px-4 text-base font-semibold leading-7 text-white sm:px-6 lg:px-8">List of applications</h2>
+        <table className="mt-6 w-full whitespace-nowrap text-left">
+          <colgroup>
+            <col className="lg:w-2/12" />
+            <col className="lg:w-2/12" />
+            <col className="lg:w-2/12" />
+            <col className="lg:w-2/12" />
+            <col className="lg:w-2/12" />
+          </colgroup>
+          <thead className="border-b border-white/10 text-sm leading-6 text-white">
+            <tr>
+              <th scope="col" className="py-2 pl-4 pr-8 font-semibold sm:pl-6 lg:pl-8">
+                Name
+              </th>
+              <th scope="col" className="py-2 pl-0 pr-8 font-semibold sm:pl-6 lg:pl-8">
+                Company
+              </th>
+              <th scope="col" className="py-2 pl-0 pr-4 font-semibold sm:pl-6 lg:pl-8">
+                Website
+              </th>
+              <th scope="col" className="py-2 pl-0 pr-8 font-semibold sm:pl-6 lg:pl-8">
+                Pitching Deck
+              </th>
+              <th scope="col" className="py-2 pl-0 pr-4 font-semibold sm:pl-6 lg:pl-8">
+                Applied On
+              </th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-white/5">
+            {filtered_registrations.map((item, i) => (
+              <tr key={i}>
+                <td className="py-4 pl-4 pr-8 sm:pl-6 lg:pl-8">
+                  <div className="flex items-center">
+                    <div className="truncate overflow-hidden text-sm font-medium leading-6 text-white">{item.firstname} {item.lastname}</div>
+                  </div>
+                </td>
+                <td className="py-4 pl-4 pr-8 sm:pl-6 lg:pl-8">
+                  <div className="flex items-center">
+                    <div className="truncate overflow-hidden text-sm font-medium leading-6 text-white">{item.company}</div>
+                  </div>
+                </td>
+                <td className="py-4 pl-4 pr-8 sm:pl-6 lg:pl-8">
+                  <div className="flex items-center">
+                    <div className="truncate overflow-hidden text-sm font-medium leading-6 text-white underline">
+                      <a href={item.website} target="_blank">{item.website}</a>
+                    </div>
+                  </div>
+                </td>
+                <td className="py-4 pl-4 pr-8 sm:pl-6 lg:pl-8">
+                  <div className="flex items-center">
+                    <div className="truncate overflow-hidden text-sm font-medium leading-6 text-white underline">
+                      <a href={item.pitching_deck} target="_blank">Link</a>
+                    </div>
+                  </div>
+                </td>
+                <td className="py-4 pl-4 pr-8 sm:pl-6 lg:pl-8">
+                  <div className="flex items-center">
+                    <div className="truncate overflow-hidden text-sm font-medium leading-6 text-white">{item.applied_on}</div>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div >
+    </>
   )
 }
