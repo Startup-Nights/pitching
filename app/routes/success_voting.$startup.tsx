69999e195869885cb4dca809b7e256ca7ae2c793
Mischa Jörg <mischa@ninetyfour.ch>
2024-10-09 23:15:04 +0200

    app: success: adjust message
    Signed-off-by: Mischa Jörg <mischa@ninetyfour.ch>

--- app/routes/success_voting.$startup.tsx
+++ app/routes/success_voting.$startup.tsx
@@ -7,17 +7,15 @@ export default function Signup() {
     <div className="xl:pl-72 max-w-6xl">
       <div className="px-4 sm:px-6 lg:px-8 mb-24">
         <div className='my-12 pb-12'>
-          <h2 className="text-3xl font-semibold leading-7 text-white">Thanks for voting!</h2>
+          <h2 className="text-3xl font-semibold leading-7 text-white">Thanks for casting your vote!</h2>
           <p className="mt-4 text-sm leading-6 text-gray-400">
-            With a bit of luck, your first choice <span className="font-bold italic">{startup}</span> will be pitching at the Startup Nights!
+            With a sprinkle of luck, you’ll see <span className="font-bold italic">{startup}</span> take the stage and rock that pitch!
           </p>
-
-          <p className="mt-4 text-sm leading-6 text-gray-400">
-            Don't worry though if you haven't got a ticket yet - we got you! Use the discount code <span className="font-bold italic">thx-for-your-vote_PC15</span> when registering as a small token of appreciation.
+          <p className="mt-2 text-sm leading-6 text-gray-400">
+            If you don’t want to miss this one-of-a-kind show, grab your ticket now and enjoy a 15% discount—just use code <span className="font-bold italic">thx-for-your-vote_PC15</span>.
           </p>
-
-          <p className="mt-4 text-sm leading-6 text-gray-400">
-            See you soon!
+          <p className="mt-2 text-sm leading-6 text-gray-400">
+            See you there — it's going to be legendary!
           </p>
         </div>
       </div>
