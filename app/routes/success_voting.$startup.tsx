cf62ac2c31761789165518e6f2520cab2f8520d7
Mischa Jörg <mischa@ninetyfour.ch>
2024-10-13 12:23:10 +0200

    app: success: add navigation buttons
    Signed-off-by: Mischa Jörg <mischa@ninetyfour.ch>

--- app/routes/success_voting.$startup.tsx
+++ app/routes/success_voting.$startup.tsx
@@ -1,3 +1,4 @@
+import { ArrowRightIcon, GlobeAltIcon, RocketLaunchIcon, TicketIcon } from "@heroicons/react/24/outline"
 import { useParams } from "@remix-run/react"
 
 export default function Signup() {
@@ -5,18 +6,36 @@ export default function Signup() {
 
   return (
     <div className="xl:pl-72 max-w-6xl">
-      <div className="px-4 sm:px-6 lg:px-8 mb-24">
+      <div className="px-4 sm:px-6 lg:px-8 mb-24 max-w-xl">
         <div className='my-12 pb-12'>
           <h2 className="text-3xl font-semibold leading-7 text-white">Thanks for casting your vote!</h2>
-          <p className="mt-4 text-sm leading-6 text-gray-400">
-            With a sprinkle of luck, you’ll see <span className="font-bold italic">{startup}</span> take the stage and rock that pitch!
-          </p>
-          <p className="mt-2 text-sm leading-6 text-gray-400">
-            If you don’t want to miss this one-of-a-kind show, grab your ticket now and enjoy a 15% discount—just use code <span className="font-bold italic">thx-for-your-vote_PC15</span>.
-          </p>
-          <p className="mt-2 text-sm leading-6 text-gray-400">
-            See you there — it's going to be legendary!
-          </p>
+          <div className="mt-8 space-y-4">
+            <p className="text-sm leading-6 text-gray-400">
+              With a sprinkle of luck, you’ll see <span className="font-bold italic">{startup}</span> take the stage and rock that pitch!
+            </p>
+            <p className="text-sm leading-6 text-gray-400">
+              If you don’t want to miss this one-of-a-kind show, grab your ticket now and enjoy a 15% discount {'-'} just use code <span className="font-bold italic">thx-for-your-vote_PC15</span>.
+            </p>
+            <p className="text-sm leading-6 text-gray-400">
+              See you there — it's going to be legendary!
+            </p>
+          </div>
+          <div className="mt-8 flex flex-wrap gap-4 transition">
+            <a
+              className={"flex justify-center items-center rounded-xl gap-2 px-3 py-1.5 text-white text-sm font-semibold bg-blue-600 border-2 border-blue-600 hover:bg-blue-700 hover:border-blue-700"}
+              href="https://www.b2match.com/e/startup-nights-2024/sign-up"
+            >
+              <TicketIcon className="h-5" />
+              Onwards to the tickets!
+            </a>
+            <a
+              className={"flex justify-center items-center rounded-xl gap-2 px-3 py-1.5 text-blue-600 text-sm font-semibold border-2 border-blue-600 hover:bg-blue-600 hover:text-white"}
+              href="https://startup-nights.ch/pitching"
+            >
+              <GlobeAltIcon className="h-5" />
+              Take me back
+            </a>
+          </div>
         </div>
       </div>
     </div>
